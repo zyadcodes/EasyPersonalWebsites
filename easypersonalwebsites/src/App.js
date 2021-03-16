e4c073fd30637ba79d0915fde345fee133308473
zyadelgohary <zyadgohary@hotmail.com>
2021-03-16 14:27:32 -0700

    Initialized the app

---
+++ easypersonalwebsites/src/App.js
@@ -0,0 +1,23 @@
+// This is the main app file that is launched when the user opens the web app
+import React from "react";
+import firebase from "firebase";
+
+// Initializes the web app's connection to Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyBmwGU-zQ52KTcj-X6GHxd2QRic16V8yOM",
+  authDomain: "easypersonalwebsites.firebaseapp.com",
+  projectId: "easypersonalwebsites",
+  storageBucket: "easypersonalwebsites.appspot.com",
+  messagingSenderId: "456054332203",
+  appId: "1:456054332203:web:ef55947b16e9690963836e",
+  measurementId: "G-6QLC35S1E2",
+});
+firebase.analytics();
+
+// Declares the app
+const App = () => {
+  return <div>Hello World</div>;
+};
+
+// Exports the app
+export default App;
