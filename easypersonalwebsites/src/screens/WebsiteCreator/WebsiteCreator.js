974f609295fde1163b271d983182349e0583e48b
zyadelgohary <zyadgohary@hotmail.com>
2021-03-20 14:41:24 -0700

    Completed initial onboarding screens

--- easypersonalwebsites/src/screens/WebsiteCreator/WebsiteCreator.js
+++ easypersonalwebsites/src/screens/WebsiteCreator/WebsiteCreator.js
@@ -8,6 +8,7 @@ import { sleep } from "../../config/sleep";
 import Typist from "react-typist";
 import "react-typist/dist/Typist.css";
 import MyButton from "../../components/MyButton/MyButton";
+import FadeIn from "react-fade-in";
 
 // Creates and exports the functional component
 const WebsiteCreator = () => {
@@ -15,8 +16,7 @@ const WebsiteCreator = () => {
   const [isIntroScreenShowing, setIsIntroScreenShowing] = useState(true);
   const [isLogoTextShowing, setIsLogoTextShowing] = useState(true);
   const [isHiScreenShowing, setIsHiScreenShowing] = useState(false);
-  const [isTopLogoShowing, setIsTopLogoShowing] = useState(false);
-  const [isLetsGoVisible, setIsLetsGoVisible] = useState(false);
+  const [isHiDivShowing, setIsHiDivShowing] = useState(true);
 
   // The useEffect method will start the animation for the logo to disappear and transition up
   useEffect(() => {
@@ -25,80 +25,83 @@ const WebsiteCreator = () => {
 
   // Starts the animation according to the useEffect method
   const startAnimation = async () => {
-    await sleep(500);
-    setIsLogoTextShowing(false);
     await sleep(2000);
+    setIsLogoTextShowing(false);
+    await sleep(1000);
     setIsHiScreenShowing(true);
     setIsIntroScreenShowing(false);
-    await sleep(25);
-    setIsTopLogoShowing(true);
-    await sleep(7500);
-    setIsLetsGoVisible(true);
   };
 
   if (isIntroScreenShowing) {
     // Returns the UI of the intro screen
     return (
       <div>
-        <div
-          className={
-            isLogoTextShowing
-              ? "logoTextContainer visible"
-              : "logoTextContainer notVisible"
-          }
-        >
-          <img src={EPWLogo} className={"bigLogo"} />
-          <div className={"largestText blue bold"}>Easy Personal Websites</div>
-          <div className={"mediumText blue"}>
-            Create Your Website in 5 Minutes
+        <FadeIn visible={isLogoTextShowing}>
+          <div className={"logoTextContainer"}>
+            <img src={EPWLogo} className={"bigLogo"} />
+            <div className={"largestText blue bold"}>
+              Easy Personal Websites
+            </div>
+            <div className={"mediumText blue"}>
+              Create Your Website in 5 Minutes
+            </div>
           </div>
-        </div>
+        </FadeIn>
       </div>
     );
   } else if (isHiScreenShowing) {
     return (
-      <div className={"hiScreenContainer"}>
-        <img
-          src={EPWLogo}
-          className={
-            isTopLogoShowing ? "smallLogo visible" : "smallLogo notVisible"
-          }
-        />
-        <Typist className={"typistContainer"} cursor={{ show: false }}>
-          <a className={"largestText black bold"}>
-            Hi, my name is <span className={"blue"}>Zyad.</span>
-          </a>
-          <br />
-          <br />
-          <br />
-          <Typist.Delay ms={500} />
-          <a className={"largestText black bold"}>
-            I <span className={"blue"}>build websites.</span>
-          </a>
-          <br />
-          <br />
-          <br />
-          <Typist.Delay ms={500} />
-          <a className={"largestText black bold"}>
-            Let me help you build <span className={"blue"}>yours.</span>
-          </a>
-          <br />
-          <br />
-          <br />
-          <Typist.Delay ms={500} />
-          <a className={"largestText black bold"}>
-            It'll take <span className={"blue"}>five</span> minutes.
-          </a>
-        </Typist>
-        <div
-          className={
-            isLetsGoVisible ? "letsGoButton visible" : "letsGoButton notVisible"
-          }
-        >
-          <MyButton text={"Let's Go"} isDarkMode={false} onClick={() => {}} />
+      <FadeIn visible={isHiDivShowing}>
+        <div className={"hiScreenContainer"}>
+          <img src={EPWLogo} className={"smallLogo"} />
+          <Typist
+            className={"typistContainer"}
+            cursor={{ show: false }}
+            startDelay={550}
+          >
+            <a className={"largestText black bold"}>
+              Hi, my name is <span className={"blue"}>Zyad.</span>
+            </a>
+            <br />
+            <br />
+            <br />
+            <Typist.Delay ms={500} />
+            <a className={"largestText black bold"}>
+              I <span className={"blue"}>build websites.</span>
+            </a>
+            <br />
+            <br />
+            <br />
+            <Typist.Delay ms={500} />
+            <a className={"largestText black bold"}>
+              Let me help you build <span className={"blue"}>yours.</span>
+            </a>
+            <br />
+            <br />
+            <br />
+            <Typist.Delay ms={500} />
+            <a className={"largestText black bold"}>
+              It'll take <span className={"blue"}>five</span> minutes.
+            </a>
+          </Typist>
+          <FadeIn delay={9300}>
+            <div className={"letsGoButton"}>
+              <MyButton
+                text={"Let's Go"}
+                isDarkMode={false}
+                onClick={async () => {
+                  setIsHiDivShowing(false);
+                  await sleep(500);
+                  setIsHiScreenShowing(false);
+                }}
+              />
+            </div>
+          </FadeIn>
         </div>
-      </div>
+      </FadeIn>
     );
+  } else {
+    return <div>Hello</div>;
   }
 };
 
